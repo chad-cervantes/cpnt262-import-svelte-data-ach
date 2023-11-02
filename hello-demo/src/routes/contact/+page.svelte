39f380ff07971a042812699e05d812a0f4a1514e
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-11-02 13:02:29 -0600

    initial commit, installed svelte and tailwind npms

---
+++ hello-demo/src/routes/contact/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Contact</h1>
+<ul>
+  <li>Email: dannyboy@hotmail.ca</li>
+  <li>Cellphone: 867 - 543 - 213</li>
+</ul>
+
---
+++ hello-demo/src/routes/history/+page.svelte
@@ -0,0 +1,12 @@
+<h1>My name is Chad</h1>
+<p>I am new to the world of web development</p>
+
+<h1>This is a gallery page</h1>
+
+<script>
+  let dailyContent = 'Course Content';
+  const description = 'This course is about web development';
+</script>
+
+<h2>SAIT Svelte {dailyContent}</h2>
+<p>{description}</p>
